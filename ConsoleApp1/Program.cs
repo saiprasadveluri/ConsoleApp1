0d2dbc7eef691b03fa2ae412318298aebbd7e069
sai_prasad_veluri@yahoo.com <vsaishradha@gmail.com>
2020-06-10 13:21:47 +0530

    a first change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helo Jenkins-second test");
+            Console.WriteLine("Helo Jenkins-second test-2");
         }
     }
 }
